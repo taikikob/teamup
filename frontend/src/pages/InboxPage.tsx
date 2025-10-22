6180fd9288564757ec2b797fb734f032bb1875fc
Taiki Kobayashi <taikik@m-p66pqg7k22.local>
2025-10-21 20:21:11 -0400

    inbox page styles.

--- frontend/src/pages/InboxPage.tsx
+++ frontend/src/pages/InboxPage.tsx
@@ -23,7 +23,8 @@ function InboxPage() {
               <small>
                 {formatDistanceToNow(new Date(notif.created_at), { addSuffix: true })}
               </small>
-              <button 
+              <button
+                className="read-button" 
                 onClick={() => notif.is_read ? markAsUnread(notif.notification_id) : markAsRead(notif.notification_id)}
                 disabled={updatingIds.has(notif.notification_id)}
               >
@@ -31,12 +32,12 @@ function InboxPage() {
               </button>
               {notif.type !== 'player_removed' && notif.type !== 'player_comment_added' && notif.type !== 'team_deleted' && (
                 <Link to={`/teams/${notif.team_id}/mastery?nodeId=${notif.node_id}&taskId=${notif.task_id}`}>
-                  <button>View</button>
+                  <button className="view-button">View</button>
                 </Link>
               )}
               {notif.type === 'player_comment_added' && (
                 <Link to={`/teams/${notif.team_id}/mastery?nodeId=${notif.node_id}&taskId=${notif.task_id}&playerId=${notif.sent_from_id}`}>
-                  <button>View</button>
+                  <button className="view-button">View</button>
                 </Link>
               )}
               <hr />
