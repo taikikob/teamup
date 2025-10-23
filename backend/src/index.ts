2f67e9ff950813ce10567dd0b06fb2c1637f8b11
Taiki Kobayashi <taikik@umich.edu>
2025-10-22 22:57:31 -0400

    enabled cookies to be sent over https by adding secure and samesite parameters.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,6 +43,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 day expiration
+      secure: true,
+      sameSite: 'none' // Adjust based on your deployment (e.g., 'lax' or 'strict' for same-site requests)
     }
   })
 );
