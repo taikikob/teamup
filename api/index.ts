8f036a8c86e9aea106cdc04780620c9d7e3154eb
Taiki Kobayashi <taikik@umich.edu>
2025-10-28 12:38:07 -0400

    configuing deployment.

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,4 @@
 // api/index.ts
-import app from '../backend/dist/index.js';
+import app from '../backend/src/index';
 
-// Export your Express app directly.
-// Vercel treats this as the request handler automatically.
 export default app;
