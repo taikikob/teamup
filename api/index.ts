e7d2c86cab5d90014ac3c70c70a1306d7628ce47
Taiki Kobayashi <taikik@umich.edu>
2025-10-28 12:25:47 -0400

    changed the importing of app to be from index.js in dist, updated vercel.json.

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 // api/index.ts
-import app from '../backend/src/index';
+import app from '../backend/dist/index.js';
 
 // Export your Express app directly.
 // Vercel treats this as the request handler automatically.
