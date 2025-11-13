a50c76aaecf8edaca82dabc366ffdb36001f796a
Taiki Kobayashi <taikik@umich.edu>
2025-11-12 23:27:13 -0500

    added a gitignore in the backend for dist.

--- backend/dist/lib/accessCodeUtils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.genAccCode = genAccCode;
-function genAccCode() {
-    const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-    return Array.from({ length: 6 }, () => chars[Math.floor(Math.random() * chars.length)]).join('');
-}
